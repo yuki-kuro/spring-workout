07bb4d8d800201ab54efde8aafada9b33fce905b
kuroki <kuroki.yuki@sysystem.co.jp>
2024-10-07 18:39:34 +0900

    wq

--- src/main/java/jp/co/sysystem/springWorkout/domain/table/Userdetail.java
+++ src/main/java/jp/co/sysystem/springWorkout/domain/table/Userdetail.java
@@ -11,11 +11,11 @@ import org.springframework.data.annotation.Id;
  * @version 1.0.0 : 2020/05/13 新規作成
  */
 @Data
-public class User implements Serializable {
+public class Userdetail implements Serializable {
   private static final long serialVersionUID = 1L;
   @Id
+  private String no;
   private String id;
-  private String pass;
-  private String name;
-  private String kana;
+  private String birth;
+  private String club;
 }
